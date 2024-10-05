cbb427fa528e43a051be6f71719e8802566cea93
Debashis Mitra <mdebashis268@gmail.com>
2024-10-05 20:58:14 +0530

    first commit

---
+++ src/app/coming-soon/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+import styles from './ComingSoon.module.css';
+
+const ComingSoon: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Coming Soon</title>
+      </Head>
+      
+      <div className={styles.content}>
+        <h1>Coming Soon</h1>
+        <p>We are working hard to bring you something amazing!</p>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
